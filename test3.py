ca594be07625f80ea47d67cb7f14ef847d8d68e3
yinchao <wsyinchao@outlook.com>
2018-06-06 09:58:16 +0800

    2018.06.06

--- test3.py
+++ test3.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 ###
 # File: d:\Learn-python\test3.py
-# Last Modified: Tue Jun 05 2018
+# Last Modified: Wed Jun 06 2018
 # Modified By: yinchao
 ###
 
@@ -17,8 +17,11 @@
 ###
 
 import io
+from io import StringIO
+from io import BytesIO
 
-with open("test2.py", "r", encoding = "utf-8") as f:
+# file IO
+with open("test2.py", "r", encoding = "utf-8") as f: # 自动调用 f.close 方法关闭文件流
     # print(f.read()) # 方法 3
 
     # while True:   # 方法 2
@@ -33,6 +36,32 @@ with open("test2.py", "r", encoding = "utf-8") as f:
     
     pass
 
+# file-like object
+# 在python中返回有read()方法的对象 都被称作file-like object
+# 打开二进制文件
+# r, rb, w, wb(打开文件的几种方式)
+with open("C:/Users/yinchao/Pictures/Saved Pictures/true.jpg", "rb") as f:
+    pass
+
+
+# string IO
+# 读取内存或者网络，需要操作字符串流，或者字节流
+f = StringIO()
+len = f.write("wsyinchao")   # 创建字符流之后可以向文件流那样操作字符流了
+print(len)
+
+# 字节流
+# 操作二进制数据
+f = BytesIO()
+f.write("尹超".encode("utf-8"))  # 以字节流写入数据
+print(f.getvalue())  #有了流之后，也是想操作文件流那样操作字节流
+
+# 再次记忆
+###
+# python 在内存中使用 unicode 编码
+# "str".encode("encodeType") 转化成字节流,指定编码格式
+# b'str'.decode("encodeType") 转化成字符流，指定编码格式
+###
 
 if __name__ == "__main__":
     pass
